368504979d6981e0e4f0271723e38eaa5744e975
w-lacey <117119482+w-lacey@users.noreply.github.com>
2023-01-05 13:59:36 -0800

    Update TransferTransactionListServlet.java

--- BankAccount/src/main/java/com/account/TransferTransactionListServlet.java
+++ BankAccount/src/main/java/com/account/TransferTransactionListServlet.java
@@ -26,7 +26,7 @@ public class TransferTransactionListServlet extends HttpServlet {
         super();
     }
 
-
+    //
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		HttpSession session = request.getSession();
